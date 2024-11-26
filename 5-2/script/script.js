d3182b425d8ed0d1f047ad82237664523c4315d3
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2024-11-26 10:55:27 +0900

    5-2の完成

--- 5-2/script/script.js
+++ 5-2/script/script.js
@@ -2,7 +2,14 @@ new Vue({
   el: '#app',
   data() {
     return {
-
+      menus:[{label:'TOP',
+              path:'./index.html'},
+            {label:'ABOUT',
+              path:'./about.html'},
+            {label:'SCHEDULE',
+              path:'./schebule.html'},
+            {label:'CONTACT',
+              path:'./content.html'}]
     };
   }
 });
